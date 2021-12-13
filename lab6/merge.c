3fe69c0c292d483c409e80385cee2536baf980a0
azibf <58919103+azibf@users.noreply.github.com>
2021-12-13 21:26:58 +0300

    Update merge.c

--- lab6/merge.c
+++ lab6/merge.c
@@ -9,34 +9,22 @@
 void Merge_sort(int N, int *pmas)
 {
     int *a1 = NULL, *a2 = NULL;
-    int c1 = 1, c2 = 0;
-    a1 = (int *) malloc(c1 * sizeof(void*));
-    a2 = (int *) malloc(c2 * sizeof(void*));
-    a1[0] = *pmas;
-    for (int i = 1; i < N; ++i)
-    {
-        if (*(pmas + i) > *(pmas + i - 1))
-        {
-            ++c1;
-            a1 = (int *) realloc(a1, c1 * sizeof(void*));
-            a1[c1 - 1] = *(pmas + i);
-        }
-        else
-        {
-            ++c2;
-            a2 = (int *) realloc(a2, c2 * sizeof(void*));
-            a2[c2 - 1] = *(pmas + i);
-        }
-    }
+    if ((N / 2) > 1) Merge_sort(N / 2, pmas);
+    if ((N - N / 2) > 1) Merge_sort(N - N / 2, (pmas + N / 2));
+    a1 = (int *) malloc((N / 2) * sizeof(int));
+    a2 = (int *) malloc((N - N / 2) * sizeof(int));
+    for (int i = 0; i < N / 2; ++i) a1[i] = *(pmas + i);
+    for (int i = 0; i < N - N / 2; ++i) a2[i] = *(pmas + i + N / 2);
+
     int *p1 = &a1[0], *p2 = &a2[0];
     for (int i = 0; i < N; ++i)
     {
-        if (p1 > &a1[c1 - 1])
+        if (p1 > &a1[N / 2 - 1])
         {
             *(pmas + i) = *p2;
             ++p2;
         }
-        else if (p2 > &a2[c2 - 1])
+        else if (p2 > &a2[N - N / 2 - 1])
         {
             *(pmas + i) = *p1;
             ++p1;
